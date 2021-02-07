42c57e92c7f9f3eb705651b427e34e6b7998af19
Jane42070 <jql1377219787@gmail.com>
2021-02-07 13:10:17 +0800

    no inner border

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static char *font2[] = {
 	"JoyPixels:pixelsize=21:antialias=true:autohint=true",
 	"OPPOSans-L:pixelsize=21:antialias=true:autohint=true",
 };
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
